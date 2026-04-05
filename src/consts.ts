f274433bde1438b9c21706c04cb4652702701dd0
Alice Kim <aliceykim0828@gmail.com>
2026-04-04 23:40:36 -0400

    Update site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Alice Kim';
+export const SITE_TITLE = '예희';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
